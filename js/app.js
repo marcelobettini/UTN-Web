aaed207278ad81db61f0e9f36d9f1c7c8362301e
Marcelo Bettini <marcelobettini@yahoo.com.ar>
2021-10-06 11:19:58 -0300

    load data and display cards... need to polish

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const hamburger = document.getElementById("hamburger");
 const pullDown = document.getElementById("pull-down");
+const productsContainer = document.getElementById("products-container");
+const url = "https://5fc82e232af77700165ad172.mockapi.io/api/products";
 
 hamburger.addEventListener("click", toggleMenu);
 
@@ -8,3 +10,29 @@ function toggleMenu() {
     ? pullDown.classList.remove("invisible")
     : pullDown.classList.add("invisible");
 }
+
+async function fetchData(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  loadData(data);
+}
+
+function loadData(data) {
+  data.forEach((el) => {
+    const article = document.createElement("article");
+    const h3 = document.createElement("h3");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const p3 = document.createElement("p");
+    const img = document.createElement("img");
+    h3.innerText=el.name;
+    img.src=el.image;
+    p1.innerText=el.origin;
+    p2.innerText=el.description;
+    p3.innerText=el.price;
+    article.append(h3, img,p1, p2, p3)
+    productsContainer.appendChild(article)
+  });
+}
+
+fetchData(url);
