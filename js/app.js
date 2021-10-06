5e6f8307c7d204451a056d26497598aa5557bf49
Marcelo Bettini <marcelobettini@yahoo.com.ar>
2021-10-06 13:35:17 -0300

    product card styling

--- js/app.js
+++ js/app.js
@@ -21,16 +21,20 @@ function loadData(data) {
   data.forEach((el) => {
     const article = document.createElement("article");
     const h3 = document.createElement("h3");
+    const img = document.createElement("img");
     const p1 = document.createElement("p");
     const p2 = document.createElement("p");
     const p3 = document.createElement("p");
-    const img = document.createElement("img");
+    const p4 = document.createElement("p");
+    
     h3.innerText=el.name;
     img.src=el.image;
     p1.innerText=el.origin;
+    p1.classList.add("country")
     p2.innerText=el.description;
-    p3.innerText=el.price;
-    article.append(h3, img,p1, p2, p3)
+    p3.innerText=el.presentation;
+    p4.innerText=el.price;
+    article.append(h3, img,p1, p2, p3, p4)
     productsContainer.appendChild(article)
   });
 }
